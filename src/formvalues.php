e79f09fd8afb0d9027bd56d0b445f2146c98ec5b
Anna-DECO <56017520+Anna-DECO@users.noreply.github.com>
2022-10-21 05:12:58 +1000

    Update formvalues.php

--- src/formvalues.php
+++ src/formvalues.php
@@ -6,12 +6,12 @@
         </head>
 
         <body>
-        Thanks for your submission!
-
-        You Submitted the following:
-        Photographer: <?php echo $_POST["name"]; ?> <?php echo $_POST["lastname"]; ?><br>
-        Image Location: <?php echo $_POST["town"]; ?>, <?php echo $_POST["suburb"]; ?>, <?php echo $_POST["country"]; ?><br>
-
+        <p style="padding-bottom: 0px; margin-bottom: 0px;">Thanks for your submission!</p><br>
+        <div id="uploadform">
+        <p>You Submitted the following:</p><br>
+        <p>Photographer: <?php echo $_POST["name"]; ?> <?php echo $_POST["lastname"]; ?><br>
+        <p>Image Location: <?php echo $_POST["town"]; ?>, <?php echo $_POST["suburb"]; ?>, <?php echo $_POST["country"]; ?></p><br>
+        <p>Image Upload Status:
         <?php
         $target_dir = "uploads/";
         $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
@@ -29,8 +29,10 @@
                         $uploadOk = 0;
                 }
         }
-        ?>
-        <a href="index.html">
+        ?></p><br>
+        <p style="color: black; font-size: 20px;"> Your submission will be reviewed and approved or denied for 'Anna's Photographs' collection </p>
+        </div>
+        <a href="index.html" style="margin-bottom: 20px; padding-bottom: 20px;">
              <button class="start-but" href="index.html">GO BACK TO HOME PAGE</button>
         </a>
         </body>
